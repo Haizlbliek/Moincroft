80b53ff454322949469b662b0c1316fab71b1f2e
Haizlbliek <linus.denning@gmail.com>
2026-06-06 22:23:31 -0400

    Refactor chunk meshing to be simultaneous

--- src/world/Chunk.cs
+++ src/world/Chunk.cs
@@ -115,14 +115,19 @@ public class Chunk : ChunkData {
 		return p;
 	}
 
-	private readonly List<Vertex> vertices = [];
-	private uint[] indices = new uint[6 * 6 * 512];
+	private readonly List<Vertex> opaqueVertices = [];
+	private uint[] opaqueIndices = new uint[6 * 6 * 512];
+	private readonly List<Vertex> transparentVertices = [];
+	private uint[] transparentIndices = new uint[6 * 6 * 256];
 
-	public void MeshData(RenderLayer renderLayer) {
-		this.vertices.Clear();
-		uint vertexIndex = 0;
+	public void MeshData() {
+		this.opaqueVertices.Clear();
+		uint opaqueVertexPtr = 0;
+		uint opaqueIndexPtr = 0;
 
-		uint indexPtr = 0;
+		this.transparentVertices.Clear();
+		uint transparentVertexPtr = 0;
+		uint transparentIndexPtr = 0;
 
 		ChunkData neighbourNX = this.world.GetChunk(this.cx - 1, this.cy, this.cz) ?? World.emptyChunk;
 		ChunkData neighbourPX = this.world.GetChunk(this.cx + 1, this.cy, this.cz) ?? World.emptyChunk;
@@ -131,8 +136,8 @@ public class Chunk : ChunkData {
 		ChunkData neighbourNZ = this.world.GetChunk(this.cx, this.cy, this.cz - 1) ?? World.emptyChunk;
 		ChunkData neighbourPZ = this.world.GetChunk(this.cx, this.cy, this.cz + 1) ?? World.emptyChunk;
 
-		void AddVertex(Vector3 pos, Vector2 uv, byte ao, int color = -1) {
-			this.vertices.Add(new Vertex() {
+		void AddOpaqueVertex(Vector3 pos, Vector2 uv, byte ao, int color = -1) {
+			this.opaqueVertices.Add(new Vertex() {
 				x = pos.x,
 				y = pos.y,
 				z = pos.z,
@@ -141,17 +146,40 @@ public class Chunk : ChunkData {
 				data = ao,
 				color = (uint) color,
 			});
-			vertexIndex++;
+			opaqueVertexPtr++;
 		}
 
-		void EnsureIndexCapacity(uint needed) {
-			if (needed <= this.indices.Length) return;
+		void AddTransparentVertex(Vector3 pos, Vector2 uv, byte ao, int color = -1) {
+			this.transparentVertices.Add(new Vertex() {
+				x = pos.x,
+				y = pos.y,
+				z = pos.z,
+				u = uv.x,
+				v = uv.y,
+				data = ao,
+				color = (uint) color,
+			});
+			transparentVertexPtr++;
+		}
 
-			int newSize = this.indices.Length * 2;
+		void EnsureOpaqueCapacity(uint needed) {
+			if (needed <= this.opaqueIndices.Length) return;
+
+			int newSize = this.opaqueIndices.Length * 2;
 			while (newSize < needed)
 				newSize *= 2;
 
-			Array.Resize(ref this.indices, newSize);
+			Array.Resize(ref this.opaqueIndices, newSize);
+		}
+
+		void EnsureTransparentCapacity(uint needed) {
+			if (needed <= this.transparentIndices.Length) return;
+
+			int newSize = this.transparentIndices.Length * 2;
+			while (newSize < needed)
+				newSize *= 2;
+
+			Array.Resize(ref this.transparentIndices, newSize);
 		}
 
 		BlockType GetBlockNeighbour(BlockPos localPos) {
@@ -201,7 +229,6 @@ public class Chunk : ChunkData {
 
 					Block block = BlockRegistry.GetBlock(type.Type);
 					if (!block.data.Visible) continue;
-					if (block.data.RenderLayer != renderLayer) continue;
 
 					BlockStateItem[] models = block.data.BlockStateData.GetModels(type, pos);
 
@@ -242,22 +269,6 @@ public class Chunk : ChunkData {
 
 						FaceBasis faceBasis = Preload.FaceBases[(int) quad.direction];
 						(byte ao0, byte ao1, byte ao2, byte ao3) = GetAO(pos, faceBasis.Rotated(model.rotationX, model.rotationY, model.rotationZ));
-						EnsureIndexCapacity(indexPtr + 6);
-						if (ao0 + ao3 < ao1 + ao2) {
-							this.indices[indexPtr++] = vertexIndex + 0;
-							this.indices[indexPtr++] = vertexIndex + 1;
-							this.indices[indexPtr++] = vertexIndex + 2;
-							this.indices[indexPtr++] = vertexIndex + 2;
-							this.indices[indexPtr++] = vertexIndex + 1;
-							this.indices[indexPtr++] = vertexIndex + 3;
-						} else {
-							this.indices[indexPtr++] = vertexIndex + 0;
-							this.indices[indexPtr++] = vertexIndex + 1;
-							this.indices[indexPtr++] = vertexIndex + 3;
-							this.indices[indexPtr++] = vertexIndex + 3;
-							this.indices[indexPtr++] = vertexIndex + 2;
-							this.indices[indexPtr++] = vertexIndex + 0;
-						}
 
 						Vector3 modelPos = new Vector3(x, y, z);
 						Vector3 v0 = RotateModelPos(quad.v0, model.rotationX, model.rotationY, model.rotationZ) + modelPos;
@@ -266,33 +277,74 @@ public class Chunk : ChunkData {
 						Vector3 v3 = RotateModelPos(quad.v3, model.rotationX, model.rotationY, model.rotationZ) + modelPos;
 
 						int color = quad.tintIndex == -1 ? -1 : BlockColors.GetColor(type, quad.tintIndex);
-						AddVertex(v0, quad.uv0, (byte) (ao0 | PackedLight), color);
-						AddVertex(v1, quad.uv1, (byte) (ao1 | PackedLight), color);
-						AddVertex(v2, quad.uv2, (byte) (ao2 | PackedLight), color);
-						AddVertex(v3, quad.uv3, (byte) (ao3 | PackedLight), color);
+
+						if (block.data.RenderLayer == RenderLayer.Opaque) {
+							EnsureOpaqueCapacity(opaqueIndexPtr + 6);
+							if (ao0 + ao3 < ao1 + ao2) {
+								this.opaqueIndices[opaqueIndexPtr++] = opaqueVertexPtr + 0;
+								this.opaqueIndices[opaqueIndexPtr++] = opaqueVertexPtr + 1;
+								this.opaqueIndices[opaqueIndexPtr++] = opaqueVertexPtr + 2;
+								this.opaqueIndices[opaqueIndexPtr++] = opaqueVertexPtr + 2;
+								this.opaqueIndices[opaqueIndexPtr++] = opaqueVertexPtr + 1;
+								this.opaqueIndices[opaqueIndexPtr++] = opaqueVertexPtr + 3;
+							} else {
+								this.opaqueIndices[opaqueIndexPtr++] = opaqueVertexPtr + 0;
+								this.opaqueIndices[opaqueIndexPtr++] = opaqueVertexPtr + 1;
+								this.opaqueIndices[opaqueIndexPtr++] = opaqueVertexPtr + 3;
+								this.opaqueIndices[opaqueIndexPtr++] = opaqueVertexPtr + 3;
+								this.opaqueIndices[opaqueIndexPtr++] = opaqueVertexPtr + 2;
+								this.opaqueIndices[opaqueIndexPtr++] = opaqueVertexPtr + 0;
+							}
+
+							AddOpaqueVertex(v0, quad.uv0, (byte) (ao0 | PackedLight), color);
+							AddOpaqueVertex(v1, quad.uv1, (byte) (ao1 | PackedLight), color);
+							AddOpaqueVertex(v2, quad.uv2, (byte) (ao2 | PackedLight), color);
+							AddOpaqueVertex(v3, quad.uv3, (byte) (ao3 | PackedLight), color);
+						}
+						else if (block.data.RenderLayer == RenderLayer.Transparent) {
+							EnsureTransparentCapacity(transparentIndexPtr + 6);
+							if (ao0 + ao3 < ao1 + ao2) {
+								this.transparentIndices[transparentIndexPtr++] = transparentVertexPtr + 0;
+								this.transparentIndices[transparentIndexPtr++] = transparentVertexPtr + 1;
+								this.transparentIndices[transparentIndexPtr++] = transparentVertexPtr + 2;
+								this.transparentIndices[transparentIndexPtr++] = transparentVertexPtr + 2;
+								this.transparentIndices[transparentIndexPtr++] = transparentVertexPtr + 1;
+								this.transparentIndices[transparentIndexPtr++] = transparentVertexPtr + 3;
+							} else {
+								this.transparentIndices[transparentIndexPtr++] = transparentVertexPtr + 0;
+								this.transparentIndices[transparentIndexPtr++] = transparentVertexPtr + 1;
+								this.transparentIndices[transparentIndexPtr++] = transparentVertexPtr + 3;
+								this.transparentIndices[transparentIndexPtr++] = transparentVertexPtr + 3;
+								this.transparentIndices[transparentIndexPtr++] = transparentVertexPtr + 2;
+								this.transparentIndices[transparentIndexPtr++] = transparentVertexPtr + 0;
+							}
+
+							AddTransparentVertex(v0, quad.uv0, (byte) (ao0 | PackedLight), color);
+							AddTransparentVertex(v1, quad.uv1, (byte) (ao1 | PackedLight), color);
+							AddTransparentVertex(v2, quad.uv2, (byte) (ao2 | PackedLight), color);
+							AddTransparentVertex(v3, quad.uv3, (byte) (ao3 | PackedLight), color);
+						}
 					}
 				}
 			}
 		}
 
-		if (renderLayer == RenderLayer.Opaque)
-			this.opaqueIndexCount = indexPtr;
-		else if (renderLayer == RenderLayer.Transparent)
-			this.transparentIndexCount = indexPtr;
+		this.opaqueIndexCount = opaqueIndexPtr;
+		this.transparentIndexCount = transparentIndexPtr;
 	}
 
 	public unsafe void GenerateMesh() {
+		this.MeshData();
+
 		Program.gl.BindVertexArray(this._opaqueVao);
 
-		this.MeshData(RenderLayer.Opaque);
-
 		Program.gl.BindBuffer(BufferTargetARB.ArrayBuffer, this._opaqueVbo);
-		fixed (Vertex* buf = CollectionsMarshal.AsSpan(this.vertices)) {
-			Program.gl.BufferData(BufferTargetARB.ArrayBuffer, (nuint) (this.vertices.Count * Unsafe.SizeOf<Vertex>()), buf, BufferUsageARB.StaticDraw);
+		fixed (Vertex* buf = CollectionsMarshal.AsSpan(this.opaqueVertices)) {
+			Program.gl.BufferData(BufferTargetARB.ArrayBuffer, (nuint) (this.opaqueVertices.Count * Unsafe.SizeOf<Vertex>()), buf, BufferUsageARB.StaticDraw);
 		}
 
 		Program.gl.BindBuffer(BufferTargetARB.ElementArrayBuffer, this._opaqueEbo);
-		fixed (uint* buf = this.indices) {
+		fixed (uint* buf = this.opaqueIndices) {
 			Program.gl.BufferData(
 				BufferTargetARB.ElementArrayBuffer,
 				this.opaqueIndexCount * sizeof(uint),
@@ -309,15 +361,13 @@ public class Chunk : ChunkData {
 
 		Program.gl.BindVertexArray(this._transparentVao);
 
-		this.MeshData(RenderLayer.Transparent);
-
 		Program.gl.BindBuffer(BufferTargetARB.ArrayBuffer, this._transparentVbo);
-		fixed (Vertex* buf = CollectionsMarshal.AsSpan(this.vertices)) {
-			Program.gl.BufferData(BufferTargetARB.ArrayBuffer, (nuint) (this.vertices.Count * Unsafe.SizeOf<Vertex>()), buf, BufferUsageARB.StaticDraw);
+		fixed (Vertex* buf = CollectionsMarshal.AsSpan(this.transparentVertices)) {
+			Program.gl.BufferData(BufferTargetARB.ArrayBuffer, (nuint) (this.transparentVertices.Count * Unsafe.SizeOf<Vertex>()), buf, BufferUsageARB.StaticDraw);
 		}
 
 		Program.gl.BindBuffer(BufferTargetARB.ElementArrayBuffer, this._transparentEbo);
-		fixed (uint* buf = this.indices) {
+		fixed (uint* buf = this.transparentIndices) {
 			Program.gl.BufferData(
 				BufferTargetARB.ElementArrayBuffer,
 				this.transparentIndexCount * sizeof(uint),
